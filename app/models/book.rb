99b20bc52a1ef0ffa74f790faa5cbcf713993495
EC2 Default User <ec2-user@ip-172-31-14-241.ec2.internal>
2021-07-26 16:58:52 +0000

    コメント昨日実装開始

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
 
     belongs_to :user
+    has_many :post_comments, dependent: :destroy
 
   with_options presence: true do
     validates :title
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  attachment :profile_image
+  has_many :post_comments, dependent: :destroy
+  attachment :profile_image, destroy: false
 
   validates :name, presence: true
   validates :name,length: { minimum: 2, maximum: 20 } ,uniqueness: true
