925f08b91eb0f4c904da35ab7b0d2c8fecca669b
EC2 Default User <ec2-user@ip-172-31-14-241.ec2.internal>
2021-07-27 16:39:05 +0000

    エラー文をlayoutに統括

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,15 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      redirect_to book_path(@book)
     else
-      render :show
+    @book_new = Book.new
+    @book_comment = BookComment.new
+      render 'books/show'
     end
   end
 
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,8 +1,8 @@
 class BookComment < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :book
-  
+  validates :comment, presence: true
   validates :comment, length: { maximum: 200}
-  
+
 end
