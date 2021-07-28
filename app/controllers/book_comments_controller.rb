ed68e7231fe22e0e9add1af8db120a2045f31213
EC2 Default User <ec2-user@ip-172-31-14-241.ec2.internal>
2021-07-28 03:22:46 +0000

    リファクタリングを実行した

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,13 +2,12 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
-    if @comment.save
-      redirect_to book_path(@book)
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+    redirect_back(fallback_location: root_path)
     else
     @book_new = Book.new
-    @book_comment = BookComment.new
       render 'books/show'
     end
   end
