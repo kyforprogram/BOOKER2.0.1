988104635c870b73bebdc30a78a98ee80b537899
EC2 Default User <ec2-user@ip-172-31-14-241.ec2.internal>
2021-08-07 09:09:33 +0000

    非同期通信完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,32 @@
 class BookCommentsController < ApplicationController
+  before_action :book_params
+
 
   def create
-    @book = Book.find(params[:book_id])
+
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    if @book_comment.save
-    redirect_to book_path(@book)
-    else
-    @book_new = Book.new
-      render 'books/show'
-    end
+    @book_comment.save
+    # redirect_to book_path(@book)
+    # else
+    # @book_new = Book.new
+    #   render 'books/show'
+    # end
+
   end
 
   def destroy
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
 
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment, :book_id, :user_id)
   end
+
+  def book_params
+    @book = Book.find(params[:book_id])
+  end
+
 end
